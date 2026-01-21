42e987e75b2f1757095711ce19bce8f313d314ad
kenshar <kenkar252@gmail.com>
2026-01-21 10:00:53 +0300

    Downgrade to Tailwind v3 for build compatibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -36,17 +36,6 @@ export default {
       fontFamily: {
         sans: ['Inter', 'system-ui', '-apple-system', 'sans-serif'],
       },
-      borderRadius: {
-        'none': '0',
-        'sm': '0',
-        DEFAULT: '0',
-        'md': '0',
-        'lg': '0',
-        'xl': '0',
-        '2xl': '0',
-        '3xl': '0',
-        'full': '0',
-      },
     },
   },
   plugins: [],
