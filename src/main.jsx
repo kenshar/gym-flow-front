6e5ac2e3d503e5fdde6a885c0af76f29908ff9f4
Allan Ratemo <allanisratemo@gmail.com>
2026-01-21 08:24:49 +0300

    add Chart.js for data visualization and update base path to /gym-flow-front
    - Install chart.js and react-chartjs-2 dependencies for charting capabilities
    - Update BrowserRouter basename and Vite base config for correct deployment path
    - Remove unnecessary build options from vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/gym-flow-front'>
       <AuthProvider>
         <App />
       </AuthProvider>
